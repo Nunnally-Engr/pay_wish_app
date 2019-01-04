75f36a6b0f4848c432834a5b63298fb884167c52
井上奈々 <nana@diffeasy.com>
2019-01-04 12:45:21 +0900

    ・請求画面に日付pickerを追加（作業中）

--- lib/claim.dart
+++ lib/claim.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 import 'package:flutter/material.dart';
 import 'package:src/auth.dart';
 import 'package:cloud_functions/cloud_functions.dart';
@@ -14,13 +16,98 @@ class Claim extends StatefulWidget {
   _ClaimState createState() => new _ClaimState();
 }
 
+class _InputDropdown extends StatelessWidget {
+  const _InputDropdown({
+    Key key,
+    this.child,
+    this.labelText,
+    this.valueText,
+    this.valueStyle,
+    this.onPressed }) : super(key: key);
+
+  final String labelText;
+  final String valueText;
+  final TextStyle valueStyle;
+  final VoidCallback onPressed;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: InputDecorator(
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+        baseStyle: valueStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(valueText, style: valueStyle),
+            Icon(Icons.arrow_drop_down,
+              color: Theme.of(context).brightness == Brightness.light ? Colors.grey.shade700 : Colors.white70
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DateTimePicker extends StatelessWidget {
+  const _DateTimePicker({
+    Key key,
+    this.labelText,
+    this.selectedDate,
+    this.selectDate
+  }) : super(key: key);
+
+  final String labelText;
+  final DateTime selectedDate;
+  final ValueChanged<DateTime> selectDate;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101)
+    );
+    if (picked != null && picked != selectedDate)
+      selectDate(picked);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle valueStyle = Theme.of(context).textTheme.title;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: _InputDropdown(
+            labelText: labelText,
+            valueText: DateFormat("yyyy-MM-dd").format(selectedDate),
+            valueStyle: valueStyle,
+            onPressed: () { _selectDate(context); },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class _ClaimState extends State<Claim> {
   static final formKey = new GlobalKey<FormState>();
 
   String _buyUid = '';
   String _billingUid = '';
-  String _buyItem = '';
+  String _buyDate = ''; // 支払日
+  String _buyItem = ''; // 商品
   String _authHint = '';
+
+  DateTime _fromDate = DateTime.now();
 
   void _signOut() async {
     try {
@@ -96,6 +183,16 @@ class _ClaimState extends State<Claim> {
         initialValue: _buyUid,
         validator: (val) => val.isEmpty ? '購入者を入力してください' : null,
         onSaved: (val) => _buyUid = val,
+      )),
+      padded(child: new _DateTimePicker(
+        key: new Key('購入日'),
+        labelText: '購入日',
+        selectedDate: _fromDate,
+        selectDate: (DateTime date) {
+          setState(() {
+            _fromDate = date;
+          });
+        }
       )),
       padded(child: new TextFormField(
         key: new Key('購入品'),
@@ -183,4 +280,3 @@ class _ClaimState extends State<Claim> {
     );
   }
 }
-
